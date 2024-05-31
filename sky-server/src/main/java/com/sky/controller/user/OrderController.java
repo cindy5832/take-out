d98aa5536e19a5826a3a8bc023b73171a22e111f
cindy <cindy5832@gmail.com>
2024-05-31 20:12:51 +0800

    訂單支付、歷史訂單查詢、訂單詳情、取消訂單

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,17 +1,18 @@
 package com.sky.controller.user;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("userOrderController")
 @RequestMapping("/user/order")
@@ -24,10 +25,43 @@ public class OrderController {
 
     @PostMapping("/submit")
     @ApiOperation("用戶下單")
-    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO){
-        log.info("用戶下單，參數為：{}",ordersSubmitDTO);
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO) {
+        log.info("用戶下單，參數為：{}", ordersSubmitDTO);
         OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
         return Result.success(orderSubmitVO);
+    }
+
+    @PutMapping("/payment")
+    @ApiOperation("訂單支付")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        log.info("訂單支付：{}", ordersPaymentDTO);
+        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+        log.info("生成預支付交易單：{}", orderPaymentVO);
+        return Result.success(orderPaymentVO);
+    }
+
+    @GetMapping("/historyOrders")
+    @ApiOperation("歷史訂單查詢")
+    public Result<PageResult> page(int page, int pageSize, Integer status){
+        log.info("查詢歷史訂單");
+        PageResult pageResult = orderService.pageQuery(page, pageSize, status);
+        return Result.success(pageResult);
+    }
+
+    @GetMapping("/orderDetail/{id}")
+    @ApiOperation("查詢訂單詳情")
+    public Result<OrderVO> details(@PathVariable("id") Long id){
+        log.info("查詢訂單：{} 詳情", id);
+        OrderVO orderVO = orderService.details(id);
+        return Result.success(orderVO);
+    }
+
+    @PutMapping("/cancel/{id")
+    @ApiOperation("取消訂單")
+    public Result cancel(@PathVariable("id") Long id) throws Exception{
+        log.info("用戶：{} 取消訂單", id);
+        orderService.userCancelById(id);
+        return Result.success();
     }
 
 }
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,26 +1,30 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
-import com.sky.entity.AddressBook;
-import com.sky.entity.OrderDetail;
-import com.sky.entity.Orders;
-import com.sky.entity.ShoppingCart;
+import com.sky.entity.*;
 import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.OrderBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
-import com.sky.mapper.AddressBookMapper;
-import com.sky.mapper.OrderDetailMapper;
-import com.sky.mapper.OrderMapper;
-import com.sky.mapper.ShoppingCartMapper;
+import com.sky.mapper.*;
+import com.sky.result.PageResult;
 import com.sky.service.OrderService;
+import com.sky.utils.WeChatPayUtil;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.crypto.ShortBufferException;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +43,13 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private WeChatPayUtil weChatPayUtil;
+
 
     @Override
     @Transactional
@@ -91,5 +102,120 @@ public class OrderServiceImpl implements OrderService {
                 .orderNumber(orders.getNumber())
                 .build();
         return orderSubmitVO;
+    }
+
+    @Override
+    public OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        // 登入當前登入用戶id
+        Long userId = BaseContext.getCurrentId();
+        User user = userMapper.getById(userId);
+
+        // 調用WeChat支付api，生成預支付交易單
+        JSONObject jsonObject = weChatPayUtil.pay(
+                ordersPaymentDTO.getOrderNumber(), // 商戶訂單號
+                new BigDecimal(0.01), // 支付金額，單位=元
+                "外送訂單", // 商品描述
+                user.getOpenid() // WeChat 用戶的openid
+        );
+
+        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
+            throw new OrderBusinessException("该订单已支付");
+        }
+
+        OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
+        vo.setPackageStr(jsonObject.getString("package"));
+
+        return vo;
+    }
+
+    @Override
+    public void paySuccess(String outTradeNo) {
+        // 根據訂單號查詢訂單
+        Orders ordersDB = orderMapper.getByNumber(outTradeNo);
+
+        // 根據訂單id更新訂單狀態、支付方式、支付狀態、結帳時間
+        Orders orders = Orders.builder()
+                .id(ordersDB.getId())
+                .status(Orders.TO_BE_CONFIRMED)
+                .payStatus(Orders.PAID)
+                .checkoutTime(LocalDateTime.now())
+                .build();
+
+        orderMapper.update(orders);
+    }
+
+    @Override
+    public PageResult pageQuery(int page, int pageSize, Integer status) {
+        // 設置分頁
+        PageHelper.startPage(page, pageSize);
+
+        OrdersPageQueryDTO ordersPageQueryDTO = new OrdersPageQueryDTO();
+        ordersPageQueryDTO.setUserId(BaseContext.getCurrentId());
+        ordersPageQueryDTO.setStatus(status);
+
+        // 分頁條件查詢
+        Page<Orders> pages = orderMapper.pageQuery(ordersPageQueryDTO);
+        List<OrderVO> list = new ArrayList<>();
+        // 查出訂單明細，並封裝成OrderVO響應
+        if (pages != null || pages.getTotal() > 0) {
+            for (Orders orders : pages) {
+                Long orderId = orders.getId(); // 訂單id
+                List<OrderDetail> orderDetailList = orderDetailMapper.getByOrderId(orderId);
+
+                OrderVO orderVO = new OrderVO();
+                BeanUtils.copyProperties(orders, orderVO);
+                orderVO.setOrderDetailList(orderDetailList);
+
+                list.add(orderVO);
+            }
+        }
+        return new PageResult(pages.getTotal(), list);
+    }
+
+    @Override
+    public OrderVO details(Long id) {
+        // 根據id 查詢訂單
+        Orders orders = orderMapper.getById(id);
+        // 查詢該訂單對應的菜品/套餐 明細
+        List<OrderDetail> orderDetailList = orderDetailMapper.getByOrderId(orders.getId());
+
+        // 查詢訂單及其詳情封裝到OrderVO並返回
+        OrderVO orderVO = new OrderVO();
+        BeanUtils.copyProperties(orders, orderVO);
+        orderVO.setOrderDetailList(orderDetailList);
+        return orderVO;
+    }
+
+    @Override
+    public void userCancelById(Long id) throws Exception{
+        // 根據Id查詢訂單
+        Orders orders = orderMapper.getById(id);
+        // 確認訂單是否存在
+        if(orders == null){
+            throw new OrderBusinessException(MessageConstant.ORDER_NOT_FOUND);
+        }
+        // 訂單狀態 1待付款 2待接單 3已接單 4派送中 5已完成 6已取消
+        if(orders.getStatus() > 2){
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+        Orders order = new Orders();
+        order.setId(orders.getId());
+
+        // 訂單處於待接單狀態取消，需退款
+        if(orders.getStatus().equals(Orders.TO_BE_CONFIRMED)){
+            // 呼叫微信支付退款接口
+            weChatPayUtil.refund(
+                    orders.getNumber(), // 商戶訂單號
+                    orders.getNumber(), // 商戶退款單號
+                    new BigDecimal(0.01), // 退款金額單位 元
+                    new BigDecimal(0.01) // 原訂單金額
+            );
+            order.setPayStatus(Orders.REFUND);
+        }
+        // 更新訂單狀態、取消原因、取消時間
+        order.setStatus(Orders.CANCELLED);
+        order.setCancelReason("用戶取消");
+        order.setCancelTime(LocalDateTime.now());
+        orderMapper.update(order);
     }
 }
