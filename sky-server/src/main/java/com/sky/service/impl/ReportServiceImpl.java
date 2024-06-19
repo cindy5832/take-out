50ff3e227ac045bb92652ad37c88e88b5d07aab8
cindy <cindy5832@gmail.com>
2024-06-19 20:07:48 +0800

    用戶統計、訂單統計、銷售排名

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,9 +1,14 @@
 package com.sky.service.impl;
 
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +21,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -24,16 +30,13 @@ public class ReportServiceImpl implements ReportService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
         // 存放begin~end的所有日期
-        List<LocalDate> dateList = new ArrayList<>();
-        dateList.add(begin);
-        while (!begin.equals(end)) {
-            // 日期計算
-            begin = begin.plusDays(1);
-            dateList.add(begin);
-        }
+        List<LocalDate> dateList = getLocalDatesList(begin, end);
 
         // 存放每日營業額
         List<Double> turnoverList = new ArrayList<>();
@@ -57,5 +60,123 @@ public class ReportServiceImpl implements ReportService {
                 .dateList(dates)
                 .turnoverList(money)
                 .build();
+    }
+
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        // 存放begin~end的所有日期
+        List<LocalDate> dateList = getLocalDatesList(begin, end);
+
+        // 存放每天的新增用戶數量
+        List<Integer> newUserList = new ArrayList<>();
+        // 存放每天的總用戶數量
+        List<Integer> totalUserList = new ArrayList<>();
+
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map map = new HashMap();
+            map.put("end", endTime);
+            Integer totalUser = userMapper.countByMap(map);
+
+            map.put("begin", beginTime);
+            Integer newUser = userMapper.countByMap(map);
+
+            totalUserList.add(totalUser);
+            newUserList.add(newUser);
+        }
+
+
+        String dates = StringUtils.join(dateList, ",");
+        String newUser = StringUtils.join(newUserList, ",");
+        String totalUser = StringUtils.join(totalUserList, ",");
+
+        return UserReportVO.builder()
+                .dateList(dates)
+                .newUserList(newUser)
+                .totalUserList(totalUser)
+                .build();
+    }
+
+    @Override
+    public OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end) {
+        // 存放begin~end的所有日期
+        List<LocalDate> dateList = getLocalDatesList(begin, end);
+
+        List<Integer> orderCountList = new ArrayList<>();
+        List<Integer> validOrderCountList = new ArrayList<>();
+
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            // 查詢訂單總數
+            Integer orderCount = getOrderCount(beginTime, endTime, null);
+            orderCountList.add(orderCount);
+
+            // 查詢每天有效訂單數
+            Integer validOrderCount = getOrderCount(beginTime, endTime, Orders.COMPLETED);
+            validOrderCountList.add(validOrderCount);
+        }
+
+        Integer totalOrderCount = orderCountList.stream().reduce(Integer::sum).get();
+        Integer totalValidCount = validOrderCountList.stream().reduce(Integer::sum).get();
+
+        // 計算訂單完成率
+        Double orderCompleteRate = 0.0;
+        if (totalOrderCount != 0) {
+            orderCompleteRate = totalValidCount.doubleValue() / totalOrderCount;
+        }
+
+        String dates = StringUtils.join(dateList, ",");
+        String orderCount = StringUtils.join(orderCountList, ",");
+        String validCount = StringUtils.join(validOrderCountList, ",");
+
+        return OrderReportVO.builder()
+                .dateList(dates)
+                .orderCountList(orderCount)
+                .validOrderCountList(validCount)
+                .validOrderCount(totalValidCount)
+                .totalOrderCount(totalOrderCount)
+                .orderCompletionRate(orderCompleteRate)
+                .build();
+    }
+
+    @Override
+    public SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end) {
+        LocalDateTime beginTime = LocalDateTime.of(begin, LocalTime.MIN);
+        LocalDateTime endTime = LocalDateTime.of(end, LocalTime.MAX);
+        List<GoodsSalesDTO> salesTop10 = orderMapper.getSalesTop10(beginTime, endTime);
+
+        List<String> names = salesTop10.stream().map(GoodsSalesDTO::getName).collect(Collectors.toList());
+        String nameList = StringUtils.join(names, ",");
+        List<Integer> numbers = salesTop10.stream().map(GoodsSalesDTO::getNumber).collect(Collectors.toList());
+        String numberList = StringUtils.join(numbers, ",");
+        return SalesTop10ReportVO
+                .builder()
+                .nameList(nameList)
+                .numberList(numberList)
+                .build();
+    }
+
+    private List<LocalDate> getLocalDatesList(LocalDate begin, LocalDate end) {
+        // 存放begin~end的所有日期
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+        while (!begin.equals(end)) {
+            // 日期計算
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        return dateList;
+    }
+
+    private Integer getOrderCount(LocalDateTime begin, LocalDateTime end, Integer status) {
+        Map map = new HashMap();
+        map.put("begin", begin);
+        map.put("end", end);
+        map.put("status", status);
+        return orderMapper.countByMap(map);
     }
 }
